2b8d2a32192643c491afb816cfc5587a857cc4ca
Franthesco Marchesi <franthescoaraujo@gmail.com>
2024-02-24 15:38:02 -0300

    Notification pattern para Products

--- src/domain/product/entity/product_interface.ts
+++ src/domain/product/entity/product_interface.ts
@@ -1,7 +1,6 @@
 import Notification from "../../shared/notification/notification";
 
-export default interface ProductInterface{
-
+export default interface ProductInterface {
     getId(): string;
     get name(): string;
     changeName(name: string): void;
